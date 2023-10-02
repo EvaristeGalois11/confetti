3d480c8066caea291fe42d801a81b610b0eae85e
Claudio Nave <claudionave97@gmail.com>
2023-10-02 19:30:48 +0200

    Add last part

--- edl-generator.sh
+++ edl-generator.sh
@@ -4,11 +4,15 @@ CLIP_LENGTH=10
 
 split_file() {
     FILENAME="$1"
-    DURATION=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$FILENAME" | cut -d '.' -f1)
-    NUMBER_OF_CLIPS=$((DURATION / CLIP_LENGTH))
+    DURATION_RAW=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$FILENAME")
+    DURATION=$(echo "$DURATION_RAW" | cut -d '.' -f1)
+    NUMBER_OF_CLIPS=$((DURATION / CLIP_LENGTH - 1))
     for i in $(seq 0 $NUMBER_OF_CLIPS); do
         echo "$FILENAME,$((i * CLIP_LENGTH)),$CLIP_LENGTH" >> playlist-temp.edl
     done
+    LAST=$((DURATION % CLIP_LENGTH))
+    LAST_SPARE=$(echo "$DURATION_RAW" | cut -d '.' -f2)
+    echo "$FILENAME,$(((NUMBER_OF_CLIPS + 1) * CLIP_LENGTH)),$LAST.$LAST_SPARE" >> playlist-temp.edl
 }
 
 for file in * ; do
