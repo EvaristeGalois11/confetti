0334d2840034121ab35438e6e7634c115a27b7e5
Claudio Nave <claudionave97@gmail.com>
2023-10-02 18:50:46 +0200

    Fix shellcheck errors

--- edl-generator.sh
+++ edl-generator.sh
@@ -2,17 +2,17 @@
 
 CLIP_LENGTH=10
 
-function split_file() {
+split_file() {
     FILENAME="$1"
     DURATION=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$FILENAME" | cut -d '.' -f1)
-    NUMBER_OF_CLIPS=$(($DURATION / $CLIP_LENGTH + 1))
-    for (( i = 0; i < $NUMBER_OF_CLIPS; i++ )); do
+    NUMBER_OF_CLIPS=$((DURATION / CLIP_LENGTH))
+    for i in $(seq 0 $NUMBER_OF_CLIPS); do
         echo "$FILENAME,$((i * CLIP_LENGTH)),$CLIP_LENGTH" >> playlist-temp.edl
     done
 }
 
 for file in * ; do
-    if file -i $file | grep -q video; then
+    if file -i "$file" | grep -q video; then
         split_file "$file"
     fi
 done
