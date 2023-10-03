ab8c16569ab86f01d8d6bb7acf93a0fada97ea42
Claudio Nave <claudionave97@gmail.com>
2023-10-03 12:04:44 +0200

    Format confetti.sh

--- confetti.sh
+++ confetti.sh
@@ -3,25 +3,25 @@
 CLIP_LENGTH=10
 
 split_file() {
-    FILENAME="$1"
-    DURATION_RAW=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$FILENAME")
-    DURATION=$(echo "$DURATION_RAW" | cut -d '.' -f1)
-    NUMBER_OF_CLIPS=$((DURATION / CLIP_LENGTH - 1))
-    for i in $(seq 0 $NUMBER_OF_CLIPS); do
-        echo "$FILENAME,$((i * CLIP_LENGTH)),$CLIP_LENGTH" >> playlist-temp.edl
-    done
-    LAST=$((DURATION % CLIP_LENGTH))
-    echo "$FILENAME,$(((NUMBER_OF_CLIPS + 1) * CLIP_LENGTH)),$LAST" >> playlist-temp.edl
+	FILENAME="$1"
+	DURATION_RAW=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$FILENAME")
+	DURATION=$(echo "$DURATION_RAW" | cut -d '.' -f1)
+	NUMBER_OF_CLIPS=$((DURATION / CLIP_LENGTH - 1))
+	for i in $(seq 0 $NUMBER_OF_CLIPS); do
+		echo "$FILENAME,$((i * CLIP_LENGTH)),$CLIP_LENGTH" >>playlist-temp.edl
+	done
+	LAST=$((DURATION % CLIP_LENGTH))
+	echo "$FILENAME,$(((NUMBER_OF_CLIPS + 1) * CLIP_LENGTH)),$LAST" >>playlist-temp.edl
 }
 
-for file in * ; do
-    if file -i "$file" | grep -q video; then
-        split_file "$file"
-    fi
+for file in *; do
+	if file -i "$file" | grep -q video; then
+		split_file "$file"
+	fi
 done
 
-echo '# mpv EDL v0' > playlist.edl
-shuf playlist-temp.edl >> playlist.edl
+echo '# mpv EDL v0' >playlist.edl
+shuf playlist-temp.edl >>playlist.edl
 rm playlist-temp.edl
 
 mpv playlist.edl
